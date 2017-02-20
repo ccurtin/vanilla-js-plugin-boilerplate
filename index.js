dcecf9da2a208bfa405d8c90b9429cdb1fcab15d
ccurtin <work@christopherjamescurtin.com>
2017-02-20 02:37:31 -0500

    Init Commit - extended template

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 ( function( root, factory ) {
-
     var pluginName = 'PluginNameHere';
-
     if ( typeof define === 'function' && define.amd ) {
         define( [], factory( pluginName ) );
     } else if ( typeof exports === 'object' ) {
@@ -14,7 +12,8 @@
     'use strict';
 
     var defaults = {
-        selector: '.yourSelector',
+        selector: '[data-visibility]',
+        htmlClass: [ pluginName ],
         someDefaultOption: 'foo',
         classToAdd: "new-class-name"
     };
@@ -64,33 +63,56 @@
     Plugin.prototype = {
         init: function() {
             // find all matching DOM elements.
-            // makes `.selectors` object available to instance.
             this.selectors = document.querySelectorAll( this.options.selector )
-            for ( var i = 0; i < this.selectors.length; i++ ) {
-                var selector = this.selectors[ i ]
-                    // Do something w/ each matched selector node.
-                selector.classList.add( this.options.classToAdd )
-                    // do something
-            }
+            // execute code if selectors are found, otherwise rollback and destroy instance
+            !!this.selectors.length ? this.render() : this.destroy()
         }, // #! init
         destroy: function() {
             // Remove any event listeners and undo any "init" actions here...
+            // when destory is called w/o ever calling the render method
+            if ( !this.selectors.length ) {
+                console.log( pluginName + ' could not find any matching elements' );
+            }
+            // when destroy is called after already being initialized and rendered
+            if ( !!this.selectors.length ) {
+                for ( var i = 0; i < this.selectors.length; i++ ) {
+                    var selector = this.selectors[ i ]
+                    // Do something w/ each matched selector node.
+                    selector.classList.remove( this.options.classToAdd )
+                    // do other stuff to each matched node...
+                }
+                // Remove class from HTML element to activate conditional CSS
+                document.documentElement.classList.remove( this.options.htmlClass );
+                console.log( pluginName + " Destroyed Successfully" )
+            }
         },
-        doSomething: function( someData ) {
-                console.log( someData )
+
+        render: function() {
+            console.log( "Initialized Successfully" )
+            // Add class to HTML element to activate conditional CSS
+            document.documentElement.classList.add( this.options.htmlClass );
+
+            for ( var i = 0; i < this.selectors.length; i++ ) {
+                var selector = this.selectors[ i ]
+                // Do something w/ each matched selector node.
+                selector.classList.add( this.options.classToAdd )
+                // do other stuff to each matched node...
+            }
+        }, // #! render
+
+        doSomething: function( statement ) {
+                console.log( statement )
             } // #! doSomething
     };
     return Plugin;
 } ) );
 
-
 /**************
     EXAMPLE:
 **************/
-
 //// create new Plugin instance
 // var pluginInstance = new PluginNameHere({
-//     selector: ".box",
+//     selector: ".box-1",
 //     someDefaultOption: 'foo2',
 //     classToAdd: "custom-new-class-name",
 // })
